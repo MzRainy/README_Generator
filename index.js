e5129f342e5e36f6bdc4340bdd206577ef3553fb
Rainy <142264832+MzRainy@users.noreply.github.com>
2023-10-24 23:39:32 -0500

    generate markdown

--- index.js
+++ index.js
@@ -2,26 +2,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
+const generateMarkdown = require("../utils/generateMarkdown");
 
-
-// TODO: Create an array of questions for user input
-// README.md is generated with the title of my project and sections entitled Description, Table of Contents,
-// Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-const questions = [];
-inquirer
-  .prompt([
+const questions = [
     {
         // Project Title
         type: 'input',
@@ -43,10 +26,9 @@ inquirer
       },
       {
         //Installation
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'contact',
-        choices: ['email', 'phone', 'telekinesis'],
+        type: 'input',
+        message: 'How do you install your application?',
+        name: 'install',
       },
       {
         // License
@@ -57,53 +39,46 @@ inquirer
       },
       {
         //Usage
-        type: 'list',
-        message: 'What is your preferred method of communication?',
+        type: 'input',
+        message: 'What is the usage purposes of your application?',
         name: 'contact',
-        choices: ['email', 'phone', 'telekinesis'],
       },
       {
         //Contributing
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'contact',
-        choices: ['email', 'phone', 'telekinesis'],
+        type: 'input',
+        message: 'How can someone contribute?',
+        name: 'contributing',
       },
       {
         //Tests
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'contact',
-        choices: ['email', 'phone', 'telekinesis'],
+        type: 'input',
+        message: 'Are there any tests that need to be run?',
+        name: 'tests',
       },
       {
         //Questions: Github link, email
-        type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'contact',
-        choices: ['email', 'phone', 'telekinesis'],
+        type: 'input',
+        message: 'What is your github url?',
+        name: 'questions',
       },
-    ])
-    .then((data) => {
-      const filename = `${data.name.toUpperCase().split(' ').join('')}.md`;
-  
-      fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-        err ? console.log(err) : console.log('Success!')
-      );
-    });
-// TODO: Create a function to write README file
-// fs.appendFile('README.md', `${process.argv[2]}\n`, (err) =>
-// err ? console.error(err) : console.log('Commit logged!')
-// );
+      {
+        //Questions: Github link, email
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'questions',
+      },
 
+    ];
+    // Writing README.md File
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// TODO: Create a function to initialize app
-// function init() {
-//     inquirer.prompt(questions).then(responses) => {
-//         console.log('Creating professional README file:');
-//         writeToFile('./utils/READMEGEN.md',)
-//     }
-// }
-
-// Function call to initialize app
+// Initializing app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Creating Professional README.md File...");
+    writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+  });
+}
 init();
